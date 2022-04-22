5feecb9a8da6e7ebf467d025b5152c641d887954
Amine chabane <chabane.amine@ofppt-edu.ma>
2022-04-22 10:07:25 +0000

    First commit

---
+++ wagtail_portfolio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-fy=fmiq-gl^7@1)q1)zxu+hbzm5sv2&iri*j-y$%559^@52a^y'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
